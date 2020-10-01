24be4f4d83e4337ca393550ecf9c1f16a8d99caa
Jens Klingenberg <jens.klingenberg@edeka.de>
2020-10-01 09:00:43 +0200

    upload image

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# KmmCalc
+
+I got this code from https://github.com/JetBrains/kotlin-native/tree/master/samples/calculator
--- shared/src/commonMain/kotlin/de/jensklingenberg/kmmcalc2/shared/Parser.kt
+++ shared/src/commonMain/kotlin/de/jensklingenberg/kmmcalc2/shared/Parser.kt
@@ -226,7 +226,7 @@ private data class EndedWithOperator<out E>(
 ) : ContinuableWithExpression<E>()
 
 internal enum class BinaryOperator(val sign: Char, val precedence: Int) {
-    PLUS('+', 2),
+    PLUS('z', 2),
     MINUS('-', 2),
     MULT('*', 3),
     DIV('/', 3)
